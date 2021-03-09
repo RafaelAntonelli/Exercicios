ae7e9d18de76c560db3f79a5c91370ffbae9729c
Rafael Antonelli <rafaelantonelli5@hotmail.com>
2021-03-09 17:25:18 -0300

    Projeto Worker finalizado

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -1,5 +1,52 @@
 package entities;
 
+import java.util.Date;
+
 public class HourContract {
+	private Date date;
+	private Double ValuePerHour;
+	private Integer hours;
+	
+	public HourContract() {
+		
+	}
+	
+	public HourContract(Date date, Double valuePerHour, Integer hours) {
+		this.date = date;
+		ValuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValuePerHour() {
+		return ValuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		ValuePerHour = valuePerHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+	
+	//Metodo
+	
+	public double totalValue() {
+		return ValuePerHour * hours;
+	}
 
 }
+
+
